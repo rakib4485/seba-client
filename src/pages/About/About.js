d81c6cf68ce56ccaf4326e7e25cb14e6fc44cff1
xbayazid <bayazid9006@gmail.com>
2023-08-04 22:00:59 +0600

    about banner added

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,10 +2,12 @@ import React from 'react';
 import AboutCards from './AboutCards/AboutCards';
 import WelcomeText from './WelcomeText/WelcomeText';
 import Details from './Details/Details';
+import AboutBanner from './AboutBanner/AboutBanner';
 
 const About = () => {
     return (
         <div className='my-20'>
+            <AboutBanner/>
             <AboutCards/>
             <WelcomeText/>
             <Details/>
---
+++ src/pages/Contact/ContactForm/ContactForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactForm = () => {
+    return (
+        <div className='my-10'>
+            
+        </div>
+    );
+};
+
+export default ContactForm;
