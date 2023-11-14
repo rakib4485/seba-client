c97231743cd0448ad639b2cbf748c3684e36a7d3
xbayazid <bayazid9006@gmail.com>
2023-11-14 21:31:29 +0600

    appintment page error solved

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -5,15 +5,8 @@ import Login from "../pages/Login/Login";
 import Signup from "../pages/Signup/Signup";
 import About from "../pages/About/About";
 import Contact from "../pages/Contact/Contact";
-<<<<<<< HEAD
 import Meditation from "../pages/Meditation/Meditation";
 import Appointment from "../pages/Appointment/Appointment";
-=======
-import Blog from "../pages/Blog/Blog/Blog";
-import BlogDetails from "../pages/Blog/BlogDetails/BlogDetails";
-import Posts from "../pages/Posts/Posts/Posts";
-import PrivateRoutes from "./PrivateRoutes";
->>>>>>> 3afa60bddf771a632ebef87d6b22c7f3d66f2c5e
 
 export const router = createBrowserRouter([
     {
