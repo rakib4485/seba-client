f0941f63584669ed08fe5f8ab825c2c3fe81b135
xbayazid <bayazid9006@gmail.com>
2023-11-19 00:27:25 +0600

    api link added

--- src/pages/Home/Services/ServiceCard.js
+++ src/pages/Home/Services/ServiceCard.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ServiceCard = ({ service }) => {
-  const { img, title, description } = service;
+  const {_id, img, title, description } = service;
   return (
     <div className="relative mb-32">
       <figure><img src={img} alt="" className=''/></figure>
       <div className="bg-[#F4F3F2] p-5 w-[80%] absolute -bottom-28 text-center">
         <h2 className="text-[#6C6C6C] text-lg">{title}</h2>
-        <p className='text-xl font-semibold my-2'>{description}</p>
-        <p className='text-[#906555] border-2 border-[#F4F3F2] border-b-[#906555] p-2 hover:text-white hover:bg-[#906555] hover:border-[#906555] w-[125px] mx-auto duration-500 '><Link>আরও পড়ুন</Link></p>
+        {/* <p className='text-xl font-semibold my-2'>{description}</p> */}
+        <p className='text-[#906555] border-2 border-[#F4F3F2] border-b-[#906555] p-2 hover:text-white hover:bg-[#906555] hover:border-[#906555] w-[125px] mx-auto duration-500 '><Link to={`/blog/${_id}`}>আরও পড়ুন</Link></p>
 
       </div>
     </div>
--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -3,55 +3,64 @@ import cupule from '../../../assets/cuppole.jpg';
 import postDivorce from '../../../assets/post-divorce.jpg';
 import children from '../../../assets/children.jpg';
 import ServiceCard from './ServiceCard';
+import { useQuery } from '@tanstack/react-query';
 
 const Services = () => {
+    const {data: services = []} = useQuery({
+        queryKey: ['service'],
+        queryFn: async() => {
+            const res = await fetch ('https://phychobuzz.vercel.app/blogs')
+            const data = await res.json()
+            return data;
+        }
+    })
 
-    const services = [
-        {
-            id: 1, 
-            img: cupule,
-            title: "গ্রুপ, থেরাপি",
-            description: "উদ্বেগ চিকিৎসা"
+    // const services = [
+    //     {
+    //         id: 1, 
+    //         img: cupule,
+    //         title: "গ্রুপ, থেরাপি",
+    //         description: "উদ্বেগ চিকিৎসা"
 
-        },
+    //     },
         
-        {
-            id: 2, 
-            img: postDivorce,
-            title: "পরিবার",
-            description: "দম্পতি থেরাপি"
+    //     {
+    //         id: 2, 
+    //         img: postDivorce,
+    //         title: "পরিবার",
+    //         description: "দম্পতি থেরাপি"
 
-        },
-        {
-            id: 3, 
-            img: children,
-            title: "পরিবার",
-            description: "পারিবারিক চর্চা"
+    //     },
+    //     {
+    //         id: 3, 
+    //         img: children,
+    //         title: "পরিবার",
+    //         description: "পারিবারিক চর্চা"
 
-        },
-        {
-            id: 4, 
-            img: children,
-            title: "ব্যক্তিগত, থেরাপি",
-            description: "বিষন্নতা থেরাপি"
+    //     },
+    //     {
+    //         id: 4, 
+    //         img: children,
+    //         title: "ব্যক্তিগত, থেরাপি",
+    //         description: "বিষন্নতা থেরাপি"
 
-        },
-        {
-            id: 5, 
-            img: children,
-            title: "গ্রুপ, থেরাপি",
-            description: "গ্রুপ, থেরাপি"
+    //     },
+    //     {
+    //         id: 5, 
+    //         img: children,
+    //         title: "গ্রুপ, থেরাপি",
+    //         description: "গ্রুপ, থেরাপি"
 
-        },
-        {
-            id: 6, 
-            img: children,
-            title: "ব্যক্তি, কোচিং",
-            description: "ব্যক্তি, কোচিং"
+    //     },
+    //     {
+    //         id: 6, 
+    //         img: children,
+    //         title: "ব্যক্তি, কোচিং",
+    //         description: "ব্যক্তি, কোচিং"
 
-        },
+    //     },
 
-    ]
+    // ]
 
     return (
         <div className='px-[6%]'>
@@ -61,7 +70,7 @@ const Services = () => {
             <div className="grid grid-cols-3 gap-6 mt-20">
                 {
                     services.map(service => <ServiceCard 
-                        key={service.id}
+                        key={service._id}
                         service={service}
                     />)
                 }
